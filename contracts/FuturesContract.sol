b81c7f1cb674c45df90476ac041e5c9221893225
Warren <wlwanpan@uwaterloo.ca>
2018-03-10 14:18:44 -0800

    updated nested create contract

--- contracts/FuturesContract.sol
+++ contracts/FuturesContract.sol
@@ -3,43 +3,62 @@ import './oraclizeAPI.sol';
 
 contract FuturesContract is usingOraclize {
 
+  enum ContractState { Short, Long }
+
   bool public isValid;
   bool public isSigned;
 
   uint256 public expiryDate;
-  uint256 public bettingPairing;
+  uint256 public bettingPairs;
+  uint256 public bettingPrice;
+  uint256 public createAt = now;
 
-  address public owner;
-  address public signer;
+  address owner;
+  address buyer;
 
   string contractCode;
   string api;
 
-  // Events
-  event LogNewOraclizeQuery(string oraclizeLog);
+  ContractState ownerPosition;
 
-  function FuturesContract(uint256 _expiryDate, uint256 _bettingPairing, string _api) public payable {
-    owner = msg.sender;
+  // Events
+  event LogOraclizeQuery(string oraclizeLog);
+  event BeforeTerminateContact(address winner, uint256 price, string status);
+
+  // Function modifiers
+  modifier onlyOwner() { require(msg.sender == owner);_; }
+  modifier notOwner() { require(msg.sender != owner);_; }
+
+  function FuturesContract(address _owner, uint256 _expiryDate, uint256 _bettingPairs, uint256 _bettingPrice, string _position, string _api) public payable {
+    require(_position == 'Short' || _position == 'Long');
+    owner = _owner;
     isValid = true;
 
     expiryDate = _expiryDate;
-    bettingPairing = _bettingPairing;
+    bettingPairs = _bettingPairs;
+    bettingPrice = _bettingPrice;
     api = _api;
+
+    if (_position == 'Short') { ownerPosition = ContractState.Short; }
+    else { ownerPosition = ContractState.Long; }
   }
 
-  function signContract() public payable {
-    require(msg.sender != owner);
-
+  function signContract(address _buyer) public payable notOwner {
     if (oraclize_getPrice("URL") > this.balance) {
-      LogNewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
+      LogOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
     }
     else {
-      LogNewOraclizeQuery("Retrieving lastest pairing...");
+      LogOraclizeQuery("Contract has been Signed");
 
       isSigned = true;
+      buyer = _buyer;
       oraclize_query(scheduled_arrivaltime + expiryDate * 3600, "URL", api);
       // Example json response: {"data":{"base":"ETH","currency":"USD","amount":"848.22"}}
     }
+  }
+
+  function cancelContract() public onlyOwner {
+    teminateContract(buyer, 'Contract has been cancelled');
   }
 
   function __callback(bytes32 _queryID, string _result) public {
@@ -47,10 +66,19 @@ contract FuturesContract is usingOraclize {
     require(msg.sender == oraclize_cbAddress());
 
     uint256 resultPairing = parseInt(_result);
-    if (resultPairing > bettingPairing) {
+    if (resultPairing >= bettingPrice && ownerPosition == ContractState.Long) {
+      // Owner Won the bet
+      teminateContract(owner, 'Contract terminated. Owner won.');
     }
     else {
+      // Owner lost the bet
+      teminateContract(buyer, 'Contract terminated. Buyer won.');
     }
+  }
+
+  function teminateContract(address _claimer, string _status) private {
+    BeforeTerminateContact(_claimer, this.balance, _status);
+    selfdestruct(_claimer);
   }
 
 }
--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -8,10 +8,12 @@ contract MarketPlace is usingOraclize {
 
   // Unscoped variables
   string public binanceBaseAPI = "https://api.binance.com/api/v3/ticker/price?symbol=";
-  string[] public allowedPairings;
+  string[] public availablePairings = [
+    'ETHEOS', 'ETHTRX', 'ETHOMG', 'ETHQTM'
+  ];
 
   uint256 constant timeStampInterval = 3*3600;
-  uint256 public totalOnlineUsers;
+  uint256 public userOnlineCount;
 
   address owner;
 
@@ -25,7 +27,6 @@ contract MarketPlace is usingOraclize {
   }
 
   address[] userAddresses;
-  uint256[] signedFuturesContactIds;
   uint256[] unsignedFuturesContactIds;
 
   // Mapping data struct
@@ -35,49 +36,81 @@ contract MarketPlace is usingOraclize {
 
   // Event Logs
 
-  // Function  Modifiers
-  modifier onlyBy(address _account) {
-    require(msg.sender == _account);
-    _;
-  }
+  // Admin Management Function Modifiers
+  modifier onlyOwner() { require(msg.sender == owner);_; }
+  // User Management Function Modifiers
+  modifier userExist() { require(users[msg.sender].exist);_; }
+  modifier notUserExist() { require(!users[msg.sender].exist);_; }
+  modifier userIsOnline() { require(users[msg.sender].status == UserLoginStatus.Online);_; }
 
-  modifier userExist() {
-    require(users[msg.sender].exist);
-    _;
-  }
-
+  // Contructor
   function MarketPlace() public {
-    // Triggers recursive callback for ETH-USD pairing prices
     owner = msg.sender;
   }
 
-  function registerUser(string _name, string _password) public {
+  function signContractOf(address _seller, address _contractAddress) public payable userExist userIsOnline {
+    // Sign contract from another User
+    require(users[_seller].exist);
+
+    User seller = users[_seller];
+    FuturesContract sellerContract = FuturesContract.at(_contractAddress);
+    sellerContract.signContract(msg.sender);
+  }
+
+  function createContract(uint256 _expiryDate, uint256 _bettingPairs, uint256 _bettingPrice, string _position)
+    public payable userExist userIsOnline {
+    // User create new unsigned contract
+
+    User currentUser = users[msg.sender];
+    string memory bettingAPI = strConcat(binanceBaseAPI, _bettingPairs);
+
+    FuturesContract newContract = (new FuturesContract).value(msg.sender)(
+        msg.sender,
+        _expiryDate,
+        _bettingPairs,
+        _bettingPrice,
+        _position,
+        bettingAPI
+      );
+
+    currentUser.futuresContractID.push(newContract.address);
+  }
+
+  // Admin Update Functions
+  function updatePairings(string _newPairing) public onlyOwner {
+    // Update Existing Pairing with ticker
+    availablePairings.push(_newPairing);
+  }
+
+  // User Account Management Function
+  function registerUser(string _name, string _password) public notUserExist {
+    // Params Validation
     require(bytes(_name).length > 3);
     require(bytes(_password).length > 3);
-    require(!users[msg.sender].exist);
-    // Register new user to contract
+
+    // Register new user to Contract
     users[msg.sender] = User({
-        exist: true,
-        name: _name,
-        status: UserLoginStatus.Offline,
-        futuresContractID: new uint256[](0),
-        passwordHash: keccak256(_password, _name)
-      });
+      exist: true,
+      name: _name,
+      status: UserLoginStatus.Offline,
+      futuresContractID: new uint256[](0),
+      passwordHash: keccak256(_password, _name)
+    });
   }
 
   function loginUser(string _name, string _password) public userExist {
     // Authenticate and set user login status to online
     require(authenticate(msg.sender, _name, _password));
     users[msg.sender].status = UserLoginStatus.Online;
-    totalOnlineUsers++;
+    userOnlineCount++;
   }
 
-  function logoutUser() public userExist onlyBy(msg.sender) {
+  function logoutUser() public userExist {
     // Authenticate and set user login status to offline
     User storage currentUser = users[msg.sender];
     require(currentUser.status == UserLoginStatus.Online);
     currentUser.status = UserLoginStatus.Offline;
-    totalOnlineUsers--;
+    userOnlineCount--;
   }
 
   function authenticate(address _user, string _name, string _password) private view returns(bool) {
